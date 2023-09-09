69ff5c27719cd90d7b17c8f2f7bd227c17527a20
ozoani <97629644+ozoani@users.noreply.github.com>
2023-09-08 20:19:38 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
 
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 'a' ; i <= 'z' ; i++)
-                putchar(i);
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
 	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar(i);
-        putchar('\n');
-        return (0);
+	putchar('\n');
+	return (0);
 }
